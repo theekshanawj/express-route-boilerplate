e2e14ed957070ee116ad51ab227df810eeb2e68e
Theekshana Wijesinghe <Theekshana.Dilshan@syscolabs.com>
2020-02-23 11:21:02 +0530

    refactor the code and comment

--- index.js
+++ index.js
@@ -2,84 +2,91 @@ const express = require('express');
 
 const { getFiles, getAbsolutePath, getFileNameAndExtension, isExtensionValid, getRoute } = require('./util');
 
-
 const app = express();
 
-const RELAVTIVE_PATH_TO_MIDDLWARES = './middleware/';
-const RELAVTIVE_PATH_TO_CONTROLLERS = './controllers/';
-const ALLOWED_FILE_EXTENSIONS = ['js'];
-const ROOT_ROUTE_NAME = 'root';
-const PORT = 3000;
-const DEFULT_PORT = 3000;
-const appListenCallback = () => {
-	console.log('App started on port:',PORT);
-}
+/**
+ * Application constants
+ */
+const ALLOWED_FILE_EXTENSIONS = ['js']; // Files allowed for controllers and middlwares
+const ROOT_ROUTE_NAME = 'root'; // Default root route name
+const DEFULT_PORT = 3000; // Default port
 
-
+// Register routes
 const registerRoutes = (basePath, pathToRoutes, rootRouteName) => {
-	try {
-		const routesFiles = getFiles(getAbsolutePath(basePath, pathToRoutes));
-		if (routesFiles && routesFiles.length > 0) {
-			routesFiles.forEach((file) => {
-				const [routesFileName, extension] = getFileNameAndExtension(file);
-				if(!isExtensionValid(extension, ALLOWED_FILE_EXTENSIONS)) return;
-				const controllerRoutes = require(pathToRoutes + routesFileName);
-				const route = getRoute(routesFileName, rootRouteName);
-				route && app.use(route, controllerRoutes);
-			});
-		}
-	} catch(err) {
-		console.log('Error in accessing files', err);
-	}
-}
+  try {
+    const routesFiles = getFiles(getAbsolutePath(basePath, pathToRoutes));
+    if (routesFiles && routesFiles.length > 0) {
+      routesFiles.forEach((file) => {
+        const [routesFileName, extension] = getFileNameAndExtension(file);
+        if (!isExtensionValid(extension, ALLOWED_FILE_EXTENSIONS)) return;
+        try {
+          // Import the route file
+          const controllerRoutes = require(pathToRoutes + routesFileName);
+          const route = getRoute(routesFileName, rootRouteName);
+          // Register the routes
+          route && app.use(route, controllerRoutes);
+        } catch (e) {
+          console.error('Error occurred importing routes', e);
+        }
+      });
+    }
+  } catch (err) {
+    console.log('Error in accessing files', err);
+  }
+};
 
+// Register middleware
 const registerMiddlware = (basePath, pathToMiddleware) => {
-	try {
-		const middlewareFiles = getFiles(getAbsolutePath(basePath, pathToMiddleware));
-		if (middlewareFiles && middlewareFiles.length > 0) {
-			middlewareFiles.forEach((file) => {
-				const [middlewareFileName, extension] = getFileNameAndExtension(file);
-				if(!isExtensionValid(extension, ALLOWED_FILE_EXTENSIONS)) return;
-				// Wrap around a another try catch to catch errors with require to avoid breaking the loop
-				try {
-					// Import the file
-					const middlewares = require(pathToMiddleware + middlewareFileName);
-					if (!middlewares) return;
-					// Add middle-wares to the App
-					Object.values(middlewares).forEach((fnc) => {
-						app.use(fnc);
-					});
-				} catch(e) {
-					console.error('Error occurred importing middleware file', e);
-				}
-			});
-		}
-	} catch(err) {
-		console.error('Error occurred', err);
-	}
-}
+  try {
+    const middlewareFiles = getFiles(getAbsolutePath(basePath, pathToMiddleware));
+    if (middlewareFiles && middlewareFiles.length > 0) {
+      middlewareFiles.forEach((file) => {
+        const [middlewareFileName, extension] = getFileNameAndExtension(file);
+        if (!isExtensionValid(extension, ALLOWED_FILE_EXTENSIONS)) return;
+        // Wrap around a another try catch to catch errors with require to avoid breaking the loop
+        try {
+          // Import the file
+          const middlewares = require(pathToMiddleware + middlewareFileName);
+          if (!middlewares) return;
+          // Add middle-wares to the App
+          Object.values(middlewares).forEach((fnc) => {
+            app.use(fnc);
+          });
+        } catch (e) {
+          console.error('Error occurred importing middleware file', e);
+        }
+      });
+    }
+  } catch (err) {
+    console.error('Error occurred', err);
+  }
+};
 
+/**
+ * Main application : InitExpress.
+ * @param {string} pathToRoutes - Specify the relative path to routes directory './controllers/'
+ * Note: Ending / is required in the path.
+ * @param {string} pathToMiddleware - Specify the relative path to middleware directory './middleware/
+ * Note: Ending / is required in the path
+ * @param {string} rootRouteName - Name of the file which is intended to use as root (/) routes of the application
+ * @param {number} port - Port the application should start on
+ * @param {Function} listenCallback - Function to be passed to express().listen callback
+ */
 const initExpress = ({ pathToRoutes, pathToMiddleware, rootRouteName, port, listenCallback }) => {
+  // pathToRoutes should be defined
+  if (!pathToRoutes) throw new Error('pathToRoutes should be defined');
 
-	// pathToRoutes should be defined
-	if (!pathToRoutes) throw new Error('pathToRoutes should be defined');
+  // Register middlware if middleware path is defined
+  // Middlware should be registered before routes
+  if (pathToMiddleware) {
+    registerMiddlware(__dirname, pathToMiddleware);
+  }
 
-	// Register middlware if middleware path is defined
-	// Middlware should be registered before routes
-	if (pathToMiddleware) {
-		registerMiddlware(__dirname, pathToMiddleware);
-	}
+  // Register the routes in the application
+  registerRoutes(__dirname, pathToRoutes, rootRouteName || ROOT_ROUTE_NAME);
 
-	// Register the routes in the application
-	registerRoutes(__dirname, pathToRoutes, rootRouteName || ROOT_ROUTE_NAME);
-
-	// Listen to defined port if defined, else default port
-	app.listen(port || DEFULT_PORT, listenCallback);
-
-}
-
-
-initExpress({ pathToRoutes: RELAVTIVE_PATH_TO_CONTROLLERS, pathToMiddleware: RELAVTIVE_PATH_TO_MIDDLWARES, rootRouteName: ROOT_ROUTE_NAME, port: PORT, listenCallback: appListenCallback})
-
+  // Listen to defined port if defined, else default port
+  app.listen(port || DEFULT_PORT, listenCallback);
+};
+initExpress({ pathToRoutes: './controllers/', pathToMiddleware: './middleware/', port: 5000, listenCallback: () => {console.log('app started')}})
 module.exports = initExpress;
-
--- util.js
+++ util.js
@@ -1,51 +1,62 @@
 const fs = require('fs');
 const path = require('path');
 
-const DOT = '.'
+const DOT = '.';
 
-// Get the files in a given path
+/**
+ *  Get files in a given path
+ */
 const getFiles = (path) => {
-
-	if (!path) return [];
-	try {
-		const files = fs.readdirSync(path);
-		return files;
-	} catch(err) {
-		console.error('Error in retrieving files', err);
-		throw err;
-	}
+  if (!path) return [];
+  try {
+    const files = fs.readdirSync(path);
+    return files;
+  } catch (err) {
+    console.error('Error in retrieving files', err);
+    throw err;
+  }
 };
 
-// Except relative path as ./directory form
-// Return basePath + relativePath. 
+/**
+ * Concat basePath and relativePath
+ * basePath - Absolute path
+ * relativePath - Relative path in the form './directory/'
+ */
 const getAbsolutePath = (basePath, relativePath) => {
-
-	if(!basePath) return "";
-	// Get the /directory from ./directory form
-	const [, directory] = (relativePath || '').split(DOT);
-	if (!directory) return "";
-	return path.join(basePath, directory); 
+  if (!basePath) return '';
+  // Get the /directory from ./directory form
+  const [, directory] = (relativePath || '').split(DOT);
+  if (!directory) return '';
+  return path.join(basePath, directory);
 };
 
-// Get the file name and extension separately
+/*
+ * Get the file name and extension separately 
+ */
 const getFileNameAndExtension = (file) => {
-	if(!file) return [null, null];
-	return file.split(DOT);
-}
+  if (!file) return [null, null];
+  return file.split(DOT);
+};
 
-// Is the extension passed included in the allowedExtension array
+/**
+ * Check if the extension is allowed 
+ * extension - Extension to be tested
+ * allowedExtensions - Array with allowed extensions
+ */
 const isExtensionValid = (extension, allowedExtensions) => {
-	if(!allowedExtensions || !Array.isArray(allowedExtensions)) return false;
-	return allowedExtensions.includes(extension);
-}
+  if (!allowedExtensions || !Array.isArray(allowedExtensions)) return false;
+  return allowedExtensions.includes(extension);
+};
 
+/**
+ *  Get the route name. if the defined route name is root route name return /
+ */
 const getRoute = (routeName, ROOT_ROUTE_NAME) => {
-	if (!routeName) return;
-	if (routeName === ROOT_ROUTE_NAME) {
-		return "/"
-	}
-	return path.join('/', routeName);
-}
-
+  if (!routeName) return;
+  if (routeName === ROOT_ROUTE_NAME) {
+    return '/';
+  }
+  return path.join('/', routeName);
+};
 
 module.exports = { getFiles, getAbsolutePath, getFileNameAndExtension, isExtensionValid, getRoute };
